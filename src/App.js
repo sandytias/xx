a1e2974d2274353d38281a19cb6b7e8c35b795eb
sandy <sandytiassasmita48@gmail.com>
2019-12-16 09:31:07 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import NavbarPage from './component/navbar'
+// import CarouselPage from './component/slider'
+import Home from './pages/home'
+import { Route } from 'react-router-dom'
+import  LoginPage from './pages/Login'
+import Register from './pages/Register'
+import { API_URL } from './support/API_URL'
+import MovieDetail from './pages/MovieDetail'
+import Axios from 'axios';
+import { connect } from 'react-redux';
+import { Login } from './redux/Action'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  componentDidMount(){
+    let username = localStorage.getItem('username')
+    if(username){
+      Axios.get(API_URL + `/users?username=${username}`)
+      .then((res) => {
+        this.props.Login(res.data[0])
+      })
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        
+      <NavbarPage/>
+      {/* <Home/> */}
+      <Route path = '/Login' component = { LoginPage } exact />
+      {/* <Route path = '/register' component = { Register } /> */}
+      <Route path = '/movie-detail' component = { MovieDetail } />
+      </div>
+    )
+  }
 }
+export default connect(null, { Login })(App);
 
-export default App;
+
+
